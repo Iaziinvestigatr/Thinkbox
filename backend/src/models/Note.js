dced1a841a8a4b6cf4fcfa70d2abdc40e56a47d7
Jonah Christopher <vjchristof@gmail.com>
2025-06-07 14:58:35 +0530

    um idk

--- backend/src/models/Note.js
+++ backend/src/models/Note.js
@@ -1,27 +1,27 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const noteSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    content: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    updatedAt: {
-        type: Date,
-        default: Date.now,
-    },
-    }, {
-    timestamps: true, // Automatically manage createdAt and updatedAt fields
-});
+
+//create schema for notes
+//create a model based on the schema
+const noteSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+        },
+        updatedAt: {
+            type: Date,
+        },
+    }, 
+    {timestamps: true} // Automatically manage createdAt and updatedAt fields
+);
 
 const Note = mongoose.model('Note', noteSchema);
 
---
+++ frontend/src/pages/noteDetailPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const noteDetailPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default noteDetailPage
